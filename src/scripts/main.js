a0a5d1adac7350bf14927fdaae53d0dd03b17aa4
Fernando Santos <fern@ndo.io>
2024-11-08 16:32:05 -0500

    Premium Gift

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,33 @@
 import tippy from "tippy.js";
 
 export const customScript = function (App) {
-// Add your client scripts here
+  // Add your client scripts here
 
-// Transaction fee tooltip
-function addTransactionFeeTooltip() {
-  const transactionFeeEl = document.querySelector(
-    ".transaction-fee-opt-in .en__field__element--checkbox"
-  );
+  // Transaction fee tooltip
+  function addTransactionFeeTooltip() {
+    const transactionFeeEl = document.querySelector(
+      ".transaction-fee-opt-in .en__field__element--checkbox"
+    );
 
-  if (!transactionFeeEl) return;
+    if (!transactionFeeEl) return;
 
-  const transactionFeeTooltip = document.createElement("div");
-  transactionFeeTooltip.classList.add("transaction-fee-tooltip");
-  transactionFeeTooltip.innerHTML = "i";
-  transactionFeeEl.appendChild(transactionFeeTooltip);
+    const transactionFeeTooltip = document.createElement("div");
+    transactionFeeTooltip.classList.add("transaction-fee-tooltip");
+    transactionFeeTooltip.innerHTML = "i";
+    transactionFeeEl.appendChild(transactionFeeTooltip);
 
-  tippy(transactionFeeTooltip, {
-    content:
-      "By checking this box, you agree to cover the transaction fee for your donation. This small additional amount helps us ensure that 100% of you donation goes directly to NPCA.",
-    allowHTML: true,
-    theme: "white",
-    placement: "top",
-    trigger: "mouseenter click",
-    interactive: true,
-    arrow: "<div class='custom-tooltip-arrow'></div>",
-    offset: [0, 20],
-  });
-}
+    tippy(transactionFeeTooltip, {
+      content:
+        "By checking this box, you agree to cover the transaction fee for your donation. This small additional amount helps us ensure that 100% of you donation goes directly to NPCA.",
+      allowHTML: true,
+      theme: "white",
+      placement: "top",
+      trigger: "mouseenter click",
+      interactive: true,
+      arrow: "<div class='custom-tooltip-arrow'></div>",
+      offset: [0, 20],
+    });
+  }
 
-addTransactionFeeTooltip();
-
+  addTransactionFeeTooltip();
 };
