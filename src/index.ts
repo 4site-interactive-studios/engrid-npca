85e93d7d9001d636990edcf1207a80b98bf275fd
Fernando Santos <fern@ndo.io>
2024-12-05 15:34:52 -0500

    Moving Exit Intent Lightbox to the Theme

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,26 @@ import { Options, App } from "@4site/engrid-scripts"; // Uses ENGrid via NPM
 import "./sass/main.scss";
 import { customScript } from "./scripts/main";
 
+if (
+  App.getPageType() == "DONATION" &&
+  App.getPageNumber() < App.getPageCount()
+) {
+  // If this is a donation page and not the thank you page, add an exit intent lightbox
+  (window as any).EngridExitIntent = {
+    enabled: true,
+    title: "Did you forget something?",
+    text: "Your support will help NPCA continue to protect and conserve our national parks, ensuring that future generations can experience and enjoy the parks that mean so much to us.",
+    buttonText: "Finish my donation",
+    buttonLink: ".body-main",
+    cookieName: "engrid-exit-intent-lightbox",
+    cookieDuration: 1,
+    triggers: {
+      visibilityState: true,
+      mousePosition: true,
+    },
+  };
+}
+
 const options: Options = {
   applePay: false,
   CapitalizeFields: true,
